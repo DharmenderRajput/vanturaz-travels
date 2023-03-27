b67395caf35015ad6f52f66107ac89daabe29b41
DharmenderRajput <51106702+DharmenderRajput@users.noreply.github.com>
2023-03-27 22:46:28 +0530

    Web Updates

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const data = [
   {
     destination: 'Italy',
-    img: './img/destination/1.png',
+    img: 'https://images.thrillophilia.com/image/upload/s--nqY478mA--/c_fill,g_center,h_270,q_auto,w_362/dpr_1.0,f_auto,fl_strip_profile/v1/images/photos/000/179/438/original/1584940115_Venice.jpg.jpg',
     tagline: '',
     places: [
       {
@@ -31,10 +31,159 @@ const data = [
     ],
   },
   {
-    destination: 'Brazil',
-    img: './img/destination/2.png',
+    destination: 'India',
+    img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_270,q_auto,w_362/v1/filestore/lmipkcy3ekz0mimt1huu5cl9foc8_chandra-mahal-city-palace.jpg',
+    tagline: 'Best of India This Summer',
+    places: [
+      {
+        name: 'Thrilling Leh Ladakh',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/0kclx0s22i4skhf8xd92d2lqglaj_LADAKH%20%282%29.png',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Manali Kasol',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/0k5yf0ofvr0mrxlsu4yhhlxv2ywp_shutterstock_1565418496.jpg',
+        stars: 4,
+        tripDuration: 4,
+      },
+      {
+        name: 'North Sikkim',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/kuyiqtojk06d8n2essg3bji48ohu_yak%20ride.jpg',
+        stars: 4,
+        tripDuration: 4,
+      },
+      {
+        name: 'Rajasthan',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/lmipkcy3ekz0mimt1huu5cl9foc8_chandra-mahal-city-palace.jpg',
+        stars: 4,
+        tripDuration: 4,
+      },
+    ],
+  },
+  {
+    destination: 'Australia',
+    img: 'https://images.thrillophilia.com/image/upload/s--41W9vrmY--/c_fill,g_center,h_270,q_auto,w_362/dpr_1.5,f_auto,fl_strip_profile/v1/images/photos/000/111/002/original/1665988974_australia_%288%29.jpg.jpg',
     tagline: '',
-    places: [],
+    places: [
+      {
+        name: 'Sydney & Gold Coast',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/f0ge77t7oh5aqu6jdzujd247g3el_Australian%20Highlights.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Great Barier Reef',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/38xn14tlqdkfx566bbba4jai7d6t_Gateway%20to%20the%20Great%20Barrier%20Reef%20%282%29.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Sydney Aquarium',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/q0q8g39vgusy8bz279hfwhsw9ool_homepage-banner.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Gold Coast Safari City',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/60nkv4mqh2ueivgrlgrsjrzqax28_hrvt2skt4vpu0hyqs4ayhrjcppgn_089d9192e6cfddb35a37b47dd9718698.jpeg',
+        stars: 5,
+        tripDuration: 4,
+      },
+    ],
+  },
+  {
+    destination: 'Thailand',
+    img: 'https://images.thrillophilia.com/image/upload/s--CDPyll8x--/c_fill,g_center,h_270,q_auto,w_362/dpr_1.5,f_auto,fl_strip_profile/v1/images/photos/000/082/500/original/1464189112_thailand-wallpaper-3.jpg.jpg',
+    tagline: 'Experiential journeys will make you a storyteller',
+    places: [
+      {
+        name: 'Bangkok Pattaya Phuket ',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/2m5ttngj0ql36hhjqu1tbznrfqhj_Best%20of%20%281%29.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Phuket And Krabi',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,dpr_1.0,f_auto,fl_progressive.strip_profile,g_center,h_450,q_auto,w_753/v1/filestore/m55noegabwaojfyvwcivov1meyx0_The%20Pearls%20of%20Andaman.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Raya Islands',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/umbb0ryypnggnpqg4yafakrvx45n_1560598714_raya1.jpg.webp',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Mahanakhon Skywalk',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/ulbivpizgazosaskwnwovhrh1h3i_z4epuytp9amcysapvhd7.webp',
+        stars: 5,
+        tripDuration: 4,
+      },
+    ],
+  },
+  {
+    destination: 'Singapore',
+    img: 'https://images.thrillophilia.com/image/upload/s--BN04Metw--/c_fill,g_center,h_270,q_auto,w_362/dpr_1.5,f_auto,fl_strip_profile/v1/images/photos/000/096/592/original/1468947611_sing.jpg.jpg',
+    tagline: '',
+    places: [
+      {
+        name: 'Jewel Changi',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/1fcny27ce7hdbnn9ufh07rdtqzrs_shutterstock_1860932044.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'DUCKtours',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/7jgsc5sfncbqa1m84kvs0i3vs3yc_shutterstock_562608484.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Singapore Night Safari',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/qrg483s6w1zd5f1gsq4bbk6ee866_shutterstock_1197924529%20%281%29.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Sentosa Fun',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/b554nf09npt5wu57wgazwsf0nf2x_Sentosa%20Gateway%20Day%20View.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+    ],
+  },
+  {
+    destination: 'Dubai',
+    img: 'https://images.thrillophilia.com/image/upload/s--PtymrmYN--/c_fill,g_center,h_270,q_auto,w_362/dpr_1.5,f_auto,fl_strip_profile/v1/images/photos/000/082/476/original/1509547965_Dubai-Wallpaper-015.jpg.jpg',
+    tagline: '',
+    places: [
+      {
+        name: 'Burj Khalifa and Dubai Aquarium',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/no2fw66mugmuhgly3l5h7erw5b57_shutterstock_1682937292.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Ain Dubai',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/lgo9lc4tex7lb9t5be0hf3x69ujh_shutterstock_1870291894.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Ferrari World ',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/hb4pgqhbrz4s3ejyy60cqoglz6ci_ferrari%20world.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+      {
+        name: 'Dhow Cruise Desert Safari',
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_450,q_auto,w_753/v1/filestore/1v0wz30twezu4zgzglutjnv2hxi4_1580470961_shutterstock_705430021.jpg',
+        stars: 5,
+        tripDuration: 4,
+      },
+    ],
   },
 ];
 
